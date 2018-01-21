cc7c4d892d69ebfe2a344e8da2bcc1f3e0af0e0f
mohamed <mohamad1325@gmail.com>
2018-01-21 22:31:42 +0200

    floating button work

--- app/src/main/java/com/example/moham/lastjoke/MainActivity.java
+++ app/src/main/java/com/example/moham/lastjoke/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.moham.lastjoke;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.wangjie.androidbucket.utils.ABTextUtil;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener{
+
+    private RapidFloatingActionLayout rfaLayout;
+    private RapidFloatingActionButton rfaBtn;
+    private RapidFloatingActionHelper rfabHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RapidFloatingActionContentLabelList rfaContent = new RapidFloatingActionContentLabelList(getApplicationContext());
+
+        rfaContent.setOnRapidFloatingActionContentLabelListListener(this);
+        List<RFACLabelItem> items = new ArrayList<>();
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("Menu1")
+                .setResId(R.drawable.ic_favourit_ic)
+                .setIconNormalColor(0xFFffb022)
+                .setIconPressedColor(0xFFffb022)
+                .setWrapper(0)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                .setLabel("Menu2")
+                .setResId(R.drawable.like)
+
+                .setWrapper(1)
+        );
+
+        rfaContent
+                .setItems(items)
+                .setIconShadowRadius(ABTextUtil.dip2px(getApplicationContext(), 5))
+                .setIconShadowColor(0xff888888)
+                .setIconShadowDy(ABTextUtil.dip2px(getApplicationContext(), 5))
+        ;
+        rfabHelper = new RapidFloatingActionHelper(
+                getApplicationContext(),
+                (RapidFloatingActionLayout) findViewById(R.id.activity_main_rfal),
+                (RapidFloatingActionButton) findViewById(R.id.activity_main_rfab),
+                rfaContent
+        ).build();
+    }
+
+    @Override
+    public void onRFACItemLabelClick(int i, RFACLabelItem rfacLabelItem) {
+        rfabHelper.toggleContent();
+        int positionIndex = 6 - i;
+        if(i==0)
+        Log.d("Main activity",positionIndex+"");
+    }
+
+    @Override
+    public void onRFACItemIconClick(int i, RFACLabelItem rfacLabelItem) {
+        rfabHelper.toggleContent();
+        int positionIndex = 6 - i;
     }
 }
